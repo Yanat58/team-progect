14218a785f330309c9e1303de5f3bccdb9fae825
Incogn1tto <ageenkopetr48@gmail.com>
2022-12-17 00:05:50 +0100

    Deleted swiper

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
 import './js/modal-windows';
-
-// swiper
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Pagination]);
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
