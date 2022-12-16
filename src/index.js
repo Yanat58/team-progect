be953b0036c35524165441b9c593b041abf77add
Incogn1tto <ageenkopetr48@gmail.com>
2022-12-16 23:43:14 +0100

    added swiper

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './js/modal-windows';
+
+// swiper
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Pagination]);
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
